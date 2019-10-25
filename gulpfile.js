091a75c21f7825b617bda733797b1dc26ec7287f
khuang-dev <khuang.dev@gmail.com>
2019-10-25 14:07:18 -0700

    added scss files (style.scss, _main.scss, _mixins.scss, _variables.scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,36 @@ const gulp = require("gulp"),
   terser = require("gulp-terser"),
   rename = require("gulp-rename"),
   browserSync = require("browser-sync"),
-  eslint = require("gulp-eslint");
+  eslint = require("gulp-eslint"),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  prettyError = require('gulp-prettyerror');
 
 
-gulp.task("lint", function(){
+gulp.task('sass', function () {
   return gulp
-      .src("js/*.js")
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+    .src('./sass/style.scss')
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer(),
+    )
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task("scripts", 
+gulp.task("lint", function () {
+  return gulp
+    .src("js/*.js")
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task("scripts",
   gulp.series("lint",
     function scripts() {
       return gulp
@@ -28,19 +46,20 @@ gulp.task("scripts",
 /**
  * Browser Sync
  */
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
-      server: {
-          baseDir: "./"
-      }
+    server: {
+      baseDir: "./"
+    }
   });
 
   gulp.watch(["*.html", "build/js/*.js", "css/*.css"])
-      .on("change", browserSync.reload);
+    .on("change", browserSync.reload);
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
   gulp.watch("js/*.js", gulp.series("scripts"));
+  gulp.watch("sass/*.scss", gulp.series("sass"));
 });
 
 gulp.task("default", gulp.parallel("browser-sync", "watch"));
--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
         .done(function (data) {
             console.log(data.results);
             $('.articles').empty();
-        
             if (data.results) {
                 data.results.splice(13, data.results.length);
             }
@@ -20,42 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             })
         })
-        // if (selected == 'home') {
-        //     $.ajax({
-        //         method: 'GET',
-        //         url: 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=eCWDoGwIaY6G5tsDbjAVN3TmgUIJ1W6S'
-        //     })
-        //         .done(function (data) {
-        //             console.log(data.results);
-        //             $('.articles').empty();
-        //             $.each(data.results, function (key, value) {
-        //                 if (value.multimedia[4]) {
-        //                     $('.articles').append("<a href='" + value.url + "' target='_blank'>" + "<li style='background-image: url(" + value.multimedia[4].url + "'>" + "<p>" + value.abstract + "</p>" + "</li>" + "</a>");
-        //                     if (data.results) {
-        //                         data.results.splice(13, data.results.length);
-        //                     }
-        //                 }
-        //             })
-        //         })
-        // } else if (selected == 'arts') {
-        //     $.ajax({
-        //         method: 'GET',
-        //         url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=eCWDoGwIaY6G5tsDbjAVN3TmgUIJ1W6S'
-        //     })
-        //         .done(function (data) {
-        //             console.log(data.results);
-        //             $('.articles').empty();
-        //             $.each(data.results, function (key, value) {
-        //                 console.log(value.multimedia)
-        //                 // if (value.multimedia.length > 0 ) {
-        //                 //     $('.articles').append("<a href='" + value.url + "' target='_blank'>" + "<li style='background-image: url(" + value.multimedia[4].url + "'>" + "<p>" + value.abstract + "</p>" + "</li>" + "</a>");
-        //                     // if (data.results) {
-        //                     //     data.results.splice(13, data.results.length);
-        //                     // }
-        //                 // }
-        //             })
-        //         })
-        // }
     });
 
 });
